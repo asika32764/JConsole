e7bc19c5897034111605ec50ee53d45bed3c6041
Simon Asika <asika32764@gmail.com>
2014-02-07 14:57:24 +0800

    Reset file structure

---
+++ src/Command/Sqlsync/Export/Export.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * @package     Joomla.Cli
+ * @subpackage  JConsole
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Command\Sqlsync\Export;
+
+
+use JConsole\Command\JCommand;
+use Sqlsync\Model\Schema;
+
+defined('JCONSOLE') or die;
+
+/**
+ * Class Export
+ *
+ * @package     Joomla.Cli
+ * @subpackage  JConsole
+ *
+ * @since       3.2
+ */
+class Export extends JCommand
+{
+	/**
+	 * An enabled flag.
+	 *
+	 * @var bool
+	 */
+	public static $isEnabled = true;
+
+	/**
+	 * Console(Argument) name.
+	 *
+	 * @var  string
+	 */
+	protected $name = 'export';
+
+	/**
+	 * The command description.
+	 *
+	 * @var  string
+	 */
+	protected $description = 'Export sql.';
+
+	/**
+	 * The usage to tell user how to use this command.
+	 *
+	 * @var string
+	 */
+	protected $usage = 'export <cmd><command></cmd> <option>[option]</option>';
+
+	/**
+	 * Configure command information.
+	 *
+	 * @return void
+	 */
+	public function configure()
+	{
+		$this->addOption(
+			array('s', 'sql'),
+			0,
+			'Use sql format to export'
+		);
+	}
+
+	/**
+	 * Execute this command.
+	 *
+	 * @return int|void
+	 */
+	protected function doExecute()
+	{
+		$type = $this->getOption('s') ? 'sql' : 'yaml';
+
+		$model = new Schema;
+
+		$path = $model->getPath($type);
+
+		if (file_exists($path))
+		{
+			$yes = $this->out()->in('Schema file exists, do you want to override it (y)es|(n)o?');
+
+			if ($yes != 'y' && $yes != 'yes')
+			{
+				$this->out('cancelled.');
+
+				return;
+			}
+		}
+
+		$model->export($type);
+
+		$this->out()->out(sprintf('Schema file dumped to: %s', $model->getState()->get('dump.path')));
+
+		/*
+		$yaml = $this->getOption('yaml');
+
+		$exporter = $yaml ? new YamlExporter : new SqlExporter;
+
+		$result = $exporter->export();
+
+		$config = \JFactory::getConfig();
+
+		$file = 'site-' . $config->get('db') . '-' . date('Y-m-d-H-i-s');
+
+		if ($yaml)
+		{
+			$file .= '.yml';
+		}
+		else
+		{
+			$file .= '.sql';
+		}
+
+		$file = JCONSOLE . '/jconsole/resource/sql/export/' . ($yaml ? 'yaml/' : '') . $file;
+
+		\JFile::write($file, $result);
+
+		$this->out()->out(sprintf('Sql file dumped to: %s', $file));
+		*/
+	}
+}
---
+++ src/Command/Sqlsync/Sqlsync.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Command\Sqlsync;
+
+use JConsole\Command\JCommand;
+use Sqlsync\Schema;
+use Symfony\Component\Yaml\Dumper as SymfonyYamlDumper;
+
+class Sqlsync extends JCommand
+{
+	public $name = 'sql';
+
+	public $description = 'SQL migration tools.';
+
+	public static $isEnabled = true;
+
+	//        public $usage = 'example <command> [option]';
+
+	public function configure()
+	{
+		parent::configure();
+	}
+
+	public function execute()
+	{
+		define('SQLSYNC_COMMAND',  __DIR__);
+
+		define('SQLSYNC_RESOURCE', JCONSOLE . '/jconsole/resource/sqlsync');
+
+		define('SQLSYNC_PROFILE',  SQLSYNC_RESOURCE);
+
+		define('SQLSYNC_LIB',      JCONSOLE . '/jconsole/vendor/Sqlsync');
+
+		jimport('joomla.filesystem.file');
+		jimport('joomla.filesystem.folder');
+
+		return parent::execute();
+	}
+}
